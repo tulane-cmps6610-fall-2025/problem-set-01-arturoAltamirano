c6a13c621f3e63133899e84137aabdf7f44e0842
Arturo Altamirano <aaltamirano@tulane.edu>
2025-08-31 11:41:55 -0500

    finalizing main and tweaking notebook

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def test_foo():
     
 def longest_run(mylist, key):
     counters = []
+    counter = 0
     #iterate by index to access individual elements
     for x in range(len(mylist)):
 
@@ -46,7 +47,9 @@ def longest_run(mylist, key):
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
+    print(longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3)
 
+test_longest_run()
 
 class Result:
     """ done """
@@ -64,7 +67,73 @@ class Result:
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
     
     
+#my testing solution is different, but this one i think is closer to what the desired outcome is
+#see the notebook if you want to see the version i worked on more/modified
 def longest_run_recursive(mylist, key):
-    pass
+
+    if len(mylist) == 0:
+        #print(f"base case 0: {Result(0, 0, 0, True), call_counter}")
+        return Result(0, 0, 0, True)
+
+    elif len(mylist) == 1:
+        if mylist[0] == key:
+            #print(f"base case 1: {Result(1, 1, 1, True), call_counter}")
+            return Result(1, 1, 1, True)
+        else:
+            #print(f"base case 1: {Result(0, 0, 0, False), call_counter}")
+            return Result(0, 0, 0, False)
+
+    mid = len(mylist) // 2
+    left = mylist[:mid]
+    right = mylist[mid:]
+
+    #print(f"left {left}")
+    #print(f"right {right}")
+
+    left_result = longest_run_recursive(left, key)
+    right_result = longest_run_recursive(right, key)
+
+    #if left side is a full house, add it to the current right left size to get the full run
+    #left = [12,12,12] and right may start with 12 
+    if left_result.is_entire_range:
+        left_size = left_result.left_size + right_result.left_size
+    
+    #otherwise left is interupted at the end of the run [12,12,3] - so set the current record 
+    else:
+        left_size = left_result.left_size
+
+    #same as left but inverted for the right size with left overlap 
+    if right_result.is_entire_range:
+        right_size = right_result.right_size + left_result.right_size
+    
+    else:
+        right_size = right_result.right_size
+ 
+    cross_run = 0
+    #trying to capture values over the middle, if the right and left size indicate that the two partitions merge together
+    if left_result.right_size > 0 and right_result.left_size > 0:
+        cross_run = left_result.right_size + right_result.left_size
+
+    #capture the longest of the results across the partitons 
+    longest_size = max(left_result.longest_size, right_result.longest_size, cross_run)
+    
+    #if the paritions all indicate entire range, we're looking at a full house
+    is_entire_range = left_result.is_entire_range and right_result.is_entire_range
+
+    #should we store longest left and longest right? unsure how to handle the values here
+    return Result(left_size, right_size, longest_size, is_entire_range)
+
+    #left_size_final = int(max(left_size_track))
+    #right_size_final = int(max(right_size_track))
+
+    #print(left_size_final, right_size_final)
+    #print(longest_size)
+
+    #return Result(left_size, right_size_final, longest_size, is_entire_range), call_counter
 
 
+## Feel free to add your own tests here.
+def test_longest_run_recursive():
+    print(longest_run_recursive([2,12,12,8,12,12,12,0,12,1], 12))
+
+test_longest_run_recursive()
